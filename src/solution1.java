67ae3b5eaa6bde9b855f7d57c5b1c83cfb774af2
ian.t.crowe@gmail.com <Mrwhippy15>
2012-10-21 18:22:44 +0100

    Added in comments

--- src/solution1.java
+++ src/solution1.java
@@ -3,5 +3,15 @@
  * Date: 21/10/12
  * Time: 18:17
  */
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 public class solution1 {
+    public static void main(String args[]){
+
+    }
 }
