9ab75ffc9f217a92626425257ab3156fe101769f
ian.t.crowe@gmail.com <Mrwhippy15>
2012-10-21 18:43:22 +0100

    Completed

--- src/solution1.java
+++ src/solution1.java
@@ -12,6 +12,10 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 public class solution1 {
     public static void main(String args[]){
-
+        int x=0;
+        for( int i= 1; i < 1000; i++){
+            if(i%3 == 0 || i%5 == 0) x=i+x;
+        }
+        System.out.println(x);
     }
 }
